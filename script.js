a4e033e1072e8b40bd7b40defbbc603b20e86c6f
yunseok.ji <yunseok.j@gmail.com>
2018-01-13 17:09:37 +0900

    index.html 완료

---
+++ script.js
@@ -0,0 +1,18 @@
+function translator(str) {
+  return str.splite('').map(function(char) {
+    var d = Hangul.disassemble(char);
+    if(d[3] && Hangul.isVowel(d[1]) && Hangul.isVowel(d[2])){
+      var tmp = d[3];
+      d[3] = d[2];
+      d[2] = tmp;
+    }
+    return Hangul.assemble(d);
+  }).join('');
+
+  window.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.change').addEventListener('click', function () {
+      var changeText = translator(document.querySelector('.original-text').value);
+      document.querySelector('.result-text').innerText = changeText;
+    });
+  });
+}
